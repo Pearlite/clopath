47325272efe65fb42077810c9be8f6b03a64d312
Marrit Zuure <marrit@gmail.com>
2017-08-21 18:18:46 +0100

    Restructured my own code to prevent scope problems and tried to fix some bugs. Network.py is currently in non-working state. Continuing with network_koichiro.py (credit to Kajikawa Koichiro) which is cleaner and more functional.

--- network.py
+++ network.py
@@ -2,22 +2,34 @@ import numpy as np
 from numpy.matlib import repmat
 import matplotlib.pyplot as plt
 
+# Activation function
+def activation(x,method,coeff):
+    if method=='exp':           # Exponential
+        y=np.exp(x/coeff)
+    elif method=='sigmoid':     # Sigmoid
+        y=1/(1+np.exp(-(x)/coeff))
+        # Can put several parameters as follows.
+        # y=1/(1+np.exp(-(x-5)/8))
+    elif method=='ReLU':        # ReLU
+        y=np.maximum(0,x)
+    return y
+
 def update(r_vec, W_mat, I_vec): # update the firing rate. r_vec is a vector (firing rates at previous timestep), W is a matrix (incoming weights to all neurons), I_vec is a vector (inputs to neurons at previous timestep)
     r_rep = np.matlib.repmat(r_vec, num_neurons, 1)
     assert W_mat.shape == r_rep.shape
     x = np.sum(W_mat*r_rep, axis=0) + I_scale*I_vec # elementwise multiplication of presynaptic firing rate with the associated weights, and adding external input. Together, this is the input vector x to the postsynaptic neuron.
-    r_post = (r_vec + dt * (-r_vec + np.exp(x/1.2)))/tau_c #calculate postsynaptic firing rate
-    return r_post, x #return new firing rate, as well as input vector x
+    r_new = (r_vec + dt * (-r_vec + activation(x,'exp',1.2)))/tau_c #calculate postsynaptic firing rate
+    return r_new #return new firing rate
 
-def update_weights(r_vec, W_mat, x, threshold_vec): #BCM. Sourcing diff eqs from https://mathematical-neuroscience.springeropen.com/articles/10.1186/s13408-017-0044-6
+def update_weights(r_vec, W_mat, threshold_vec): #BCM. Sourcing diff eqs from https://mathematical-neuroscience.springeropen.com/articles/10.1186/s13408-017-0044-6
     # update threshold theta (different for each neuron)
     threshold_vec = threshold_vec + dt * ((r_vec*(r_vec - threshold_vec))/tau_th)
 
     # update weight matrix; reshape a few things to make sure I get the outer product rather than the dot product
-    x = x.reshape(1,-1)
-    r_vec = r_vec.reshape(-1,1)
+    # x = x.reshape(1,-1)
+    r_T = r_vec.reshape(-1,1)
     threshold_vec = threshold_vec.reshape(-1,1)
-    W_mat = W_mat + dt * ((r_vec * (r_vec - threshold_vec) * x)/ tau_w)
+    W_mat = W_mat + dt * ((r_vec * (r_vec - threshold_vec) * r_T)/ tau_w)
     threshold_vec = np.squeeze(threshold_vec)
 
     # make sure weights are within bounds; reshape some things because np.fmin, np.fmax only work on arrays, not matrices
@@ -43,27 +55,23 @@ def calc_clustering_index(W_mat):
     allmean  = np.mean(W_mat) #get mean firing rate of all neurons total
     return np.divide(clustermean, allmean)
 
+### set simulation parameters
+simulation = 'normal' #options: 'explore' (varying tau_r and tau_th), 'normal' (running once) with given scaling parameters
+dt = 0.1
+num_neurons = 100
+runtime = 5000 # in ms
+num_groups = 5
+
 # scaling parameters; need to balance ratio
 # rate code time constant, BCM threshold time constant, weight time constant, input scaling, initial weight scaling,  initial BCM threshold value
 [tau_c, tau_th, tau_w, I_scale, init_threshold] = [5, 50, 1000, 2, 0] # time constants in ms
 
-#set simulation parameters
-dt = 0.1
-num_neurons = 100
-runtime = 1000 # in ms
-num_groups = 5
-threshold = np.zeros((num_neurons, int(runtime/dt)))
-threshold[:,0] = init_threshold
-
 W_scale = 0.02 #weak weights to start with so they have room to grow; multiplication factor should be small
 [W_max, W_min] = [1, 0] #weight bounds; we don't want inhibitory neurons so the lower bound should be 0
 
-W = np.empty((num_neurons, num_neurons, int(runtime/dt)))
-W[:,:,0] = np.random.rand(num_neurons, num_neurons) * W_scale
-# np.fill_diagonal(W[:,:,1], 0) #no autapses
-
 min_input_duration = int(20 / dt) #in ms
 
+# generate input vector
 idx = 0
 I = np.zeros((num_neurons, int(runtime/dt)))
 which_group = np.random.randint(0,5, int((runtime/dt)/min_input_duration))
@@ -72,86 +80,81 @@ for i in range(len(which_group)):
     # I[idx[which_group[i]]:idx[which_group[i]]+(num_neurons/num_groups), i*min_input_duration:(i+1)*min_input_duration] = 1 * np.random.rand(num_neurons/num_groups, min_input_duration)
     I[idx[which_group[i]]:idx[which_group[i]]+(num_neurons/num_groups), i*min_input_duration:(i+1)*min_input_duration] = 1
 
-r = np.zeros((num_neurons, int(runtime/dt)))
-r[:,0] = 0; #initial values of r
-# r[:,0] = np.random.rand(num_neurons)
+if simulation == 'normal':
+    #### run normal simulation ####
+    # intialize firing rates, weights, thresholds
+    r = np.zeros((num_neurons, int(runtime/dt)))
+    r[:,0] = 0; #initial values of r
+    W = np.empty((num_neurons, num_neurons, int(runtime/dt)))
+    W[:,:,0] = np.random.rand(num_neurons, num_neurons) * W_scale
+    threshold = np.zeros((num_neurons, int(runtime/dt)))
+    threshold[:,0] = init_threshold
 
-# clustering = np.zeros(int(runtime/dt)-1)
-#
-# for t in range((int(runtime/dt))-1):
-#     clustering[t] = calc_clustering_index(W[:,:,t])
-#     r[:, t+1], x = update(r[:, t], W[:,:,t], I[:,t])
-#     W[:,:,t+1], threshold[:,t+1] = update_weights(r[:, t+1], W[:,:,t], x, threshold[:,t])
-#
-# plt.plot(np.transpose(r))
-# plt.show()
-#
-# plt.plot(clustering)
-# plt.show()
-#
-# #inspect weight matrix - video
-# im = plt.imshow(W[:,:,0])
-# cb = plt.colorbar()
-# cb.set_clim(vmin=0, vmax=0.05)
-# for i in range(1, int(runtime/dt), 1000):
-#     im.set_data(W[:,:,i])
-#     cb.draw_all()
-#     plt.pause(0.0001)
-# plt.show()
+    clustering = np.zeros(int(runtime/dt)-1)
 
-# explore parameter space
-tau_c_range = range(3, 6, 1)
-tau_th_range = range(40, 60, 10)
+    # run simulation
+    for t in range((int(runtime/dt))-1):
+        clustering[t] = calc_clustering_index(W[:,:,t])
+        r[:, t+1] = update(r[:, t], W[:,:,t], I[:,t])
+        W[:,:,t+1], threshold[:,t+1] = update_weights(r[:, t+1], W[:,:,t], threshold[:,t])
 
-clustering_at_end = np.empty((len(tau_c_range), len(tau_th_range)))
+    # plot some outcomes
+    plt.plot(np.transpose(r))
+    plt.show()
 
-# initialize counters for the clustering_at_end index
-[c1, c2] = [0, 0]
-tau_w = 1000 #kept constant for now
-for tau_c in tau_c_range:
-    c2 = 0
-    for tau_th in tau_th_range:
-        #reinitialize relevant parameters - can keep input I the same
+    # plt.plot(np.transpose(threshold))
+    # plt.show()
 
-        W = np.empty((num_neurons, num_neurons, int(runtime/dt)))
-        W[:,:,0] = np.random.rand(num_neurons, num_neurons) * W_scale
+    # plt.plot(clustering)
+    # plt.show()
 
-        r = np.zeros((num_neurons, int(runtime/dt)))
-        r[:,0] = 0; #initial values of r (firing rate)
-        # r[:,0] = np.random.rand(num_neurons)
+    #inspect weight matrix - video
+    im = plt.imshow(W[:,:,0])
+    cb = plt.colorbar()
+    cb.set_clim(vmin=0, vmax=0.05)
+    for i in range(1, int(runtime/dt), 1000):
+        im.set_data(W[:,:,i])
+        cb.draw_all()
+        plt.pause(0.0001)
+    plt.show()
 
-        threshold = np.zeros((num_neurons, int(runtime/dt)))
-        threshold[:,0] = init_threshold
+elif simulation == 'explore':
+    #### explore parameter space ####
+    tau_c_range = range(1, 10, 1)
+    tau_th_range = range(10, 100, 10)
 
-        for t in range((int(runtime/dt))-1):
-            r[:, t+1], x = update(r[:, t], W[:,:,t], I[:,t])
-            W[:,:,t+1], threshold[:,t+1] = update_weights(r[:, t+1], W[:,:,t], x, threshold[:,t])
+    clustering_at_end = np.empty((len(tau_c_range), len(tau_th_range)))
 
-        clustering_at_end[c1, c2] = calc_clustering_index(W[:,:,-1])
-        c2 += 1
-    c1 += 1
+    # initialize counters for the clustering_at_end index
+    [c1, c2] = [0, 0]
+    tau_w = 1000 #kept constant for now
+    for tau_c in tau_c_range:
+        c2 = 0
+        for tau_th in tau_th_range:
+            #reset weights, firing rates, thresholds. Keep input I the same.
+            W = np.empty((num_neurons, num_neurons, int(runtime/dt)))
+            W[:,:,0] = np.random.rand(num_neurons, num_neurons) * W_scale
+            r = np.zeros((num_neurons, int(runtime/dt)))
+            r[:,0] = 0; #initial values of r (firing rate)
+            threshold = np.zeros((num_neurons, int(runtime/dt)))
+            threshold[:,0] = init_threshold
 
-im = plt.imshow(clustering_at_end)
-plt.title("Clustering index at combinations of tau_c, tau_th")
-plt.colorbar()
-plt.xlabel("tau_th")
-plt.xticks(range(len(tau_th_range)), tau_th_range)
-plt.ylabel("tau_c")
-plt.yticks(range(len(tau_c_range)), tau_c_range)
-plt.show()
+            # run simulation
+            for t in range((int(runtime/dt))-1):
+                r[:, t+1] = update(r[:, t], W[:,:,t], I[:,t])
+                W[:,:,t+1], threshold[:,t+1] = update_weights(r[:, t+1], W[:,:,t], threshold[:,t])
 
-# other plotting options
-# #inspect weight matrix - still images
-# plt.subplot(3,1,1)
-# plt.imshow(W[:,:,0])
-# plt.colorbar()
-# plt.subplot(3,1,2)
-# plt.imshow(W[:,:,int(runtime/dt)/3])
-# plt.colorbar()
-# plt.subplot(3,1,3)
-# plt.imshow(W[:,:,int(runtime/dt)-1])
-# plt.colorbar()
-# plt.show()
+            # calculate clustering index at end of each simulation
+            clustering_at_end[c1, c2] = calc_clustering_index(W[:,:,-1])
+            c2 += 1
+        c1 += 1
 
-# plt.plot(np.transpose(threshold)) #show threshold over time for all neurons
-# plt.show()
+    #plot clustering indices
+    im = plt.imshow(clustering_at_end)
+    plt.title("Clustering index at combinations of tau_c, tau_th")
+    plt.colorbar()
+    plt.xlabel("tau_th")
+    plt.xticks(range(len(tau_th_range)), tau_th_range)
+    plt.ylabel("tau_c")
+    plt.yticks(range(len(tau_c_range)), tau_c_range)
+    plt.show()
---
+++ network_koichiro.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 21 12:56:56 2017
+
+@author: koichirokajikawa
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Activation function
+def activation(x,method):
+    if method=='exp':           # Exponential
+        y=np.exp(x)
+    elif method=='sigmoid':     # Sigmoid
+        y=1/(1+np.exp(-(x)))
+        # Can put several parameters as follows.
+        # y=1/(1+np.exp(-(x-5)/8))
+    elif method=='ReLU':        # ReLU
+        y=np.maximum(0,x)
+    return y
+
+## Parameters
+# 1. Network structure
+N=100       # number of neurons
+C=5         # number of clusters
+N_c=N//C    # number of neurons in a cluster
+# 2. time constants: tau_r ≤ tau_t ≤ tau_w
+[tau_r,tau_t,tau_w] = [5, 20, 1000]
+[w_max, w_min]=[1,0]
+# 3. Coefficients
+#[w_c,s_c,ip_c]=[0.25,100,10]
+#[w_c,s_c,ip_c]=[0.02,1.2,2]
+[w_c,s_c,ip_c,th_c]=[0.02,1,10,1]
+
+## Setup arrays
+T_sec  = 5             # total time to simulate (sec)
+T      = T_sec*10**3   # total time to simulate (msec)
+dt     = 0.125         # simulation time step (msec)
+time   = np.arange(0, T+dt, dt)    # time array
+r      = np.zeros((N,len(time)))
+r[:,0] = np.random.rand(N)
+W      = np.zeros((N,N,len(time)))
+W[:,:,0]= np.random.rand(N,N)*w_c
+theta  = np.zeros((N,len(time)))
+#theta[:,0]=np.random.rand(N)
+#theta[:,0]=np.ones(N,)*np.random.rand(1)
+theta[:,0]=activation((np.dot(W[:,:,0],r[:,0])),'sigmoid')**2
+#theta[:,0]=np.ones(N,)
+
+## Inputs
+ip_time=10      # Duration of input ~ tau_r*10 (?)tau_theta
+# Define input
+ip_group=np.random.randint(0,C,int(T/ip_time))
+#I=np.zeros((N, int(T/dt)))
+I=np.ones((N, int(T/dt)))*(-10)
+for k in range(len(ip_group)):
+    I[N_c*ip_group[k]:N_c*(ip_group[k]+1),k*int(ip_time/dt):(k+1)*int(ip_time/dt)]=1*ip_c
+
+# Learning
+for t in range(len(time)-1):
+    # Set diagonal entries as 0
+    np.fill_diagonal(W[:,:,t],0)
+
+    # Postsynaptic activity
+    u=np.dot(W[:,:,t],r[:,t])+I[:,t]
+    v=activation(u*s_c,'sigmoid')
+
+    # rate update
+    r[:,t+1]=r[:,t]+(-r[:,t]+v)*dt/tau_r
+
+    # weight update
+    r_post=r[:,t].reshape(-1,1)
+#    r_pre=u.reshape(1,-1)
+    r_pre=r[:,t].reshape(1,-1)
+
+    W[:,:,t+1]=W[:,:,t]+(r_post*(r_post-theta[:,t].reshape(-1,1))*r_pre)*dt/tau_w
+#    W[:,:,t+1]=np.minimum(W[:,:,t+1],w_max*np.ones((N,N)))
+    W[:,:,t+1]=np.maximum(W[:,:,t+1],w_min*np.ones((N,N)))
+
+    # theta update
+    theta[:,t+1]=theta[:,t]+((r[:,t]**2)/th_c-theta[:,t])*dt/tau_t
+
+
+l=-2
+plt.imshow(W[:,:,l])
+plt.colorbar()
+plt.show()
+
+print(W[-2,:,:].max())
+
+plt.plot()
+plt.plot(W[0,15,::100])
+plt.plot(W[0,25,::100])
+plt.plot(theta[0,::100])
+plt.plot(r[0,::100])
